e033122723add992dbc1bc6b1268e0773f337b26
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2021-11-09 20:18:33 +0900

    update: prisma shema

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const APOLLO_SERVER_PORT = process.env.PORT ?? '4000';
+export const APOLLO_SERVER_PORT = process.env.APOLLO_SERVER_PORT ?? '4000';
 export const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN ?? '';
 export const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE ?? '';
